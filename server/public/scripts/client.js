4207a09293d5d9e2c706bd37aed949f68915c7b7
Ryan Udelhoven <Ryan@Udelhoven.net>
2018-02-08 14:01:51 -0600

    Appendingto the DOM is test functional

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,17 @@
-// Empty js file. Please fill in as needed for your project.
+console.log('js');
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('jq');
+    indexAppend();
+}
+
+function indexAppend(){
+    console.log('Appender Function');
+    var $gameUI = $('#gameUI');
+    var stringToAppend = '';
+    $gameUI.empty();
+    stringToAppend += `<p>I can do what I want</p>`
+    $gameUI.append(stringToAppend);
+  }
