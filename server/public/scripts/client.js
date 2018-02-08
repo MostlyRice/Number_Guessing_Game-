391db3fe65112e02d9002da1a8d3c7d55415f670
Ryan Udelhoven <Ryan@Udelhoven.net>
2018-02-08 14:09:35 -0600

    basic append function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,6 @@
-console.log('js');
-
 $(document).ready(onReady);
 
 function onReady(){
-    console.log('jq');
     indexAppend();
 }
 
@@ -12,6 +9,7 @@ function indexAppend(){
     var $gameUI = $('#gameUI');
     var stringToAppend = '';
     $gameUI.empty();
-    stringToAppend += `<p>I can do what I want</p>`
+    stringToAppend += `<p>I can do what I want</p>` // Add to DOM, copy this, paste below
     $gameUI.append(stringToAppend);
-  }
+}
+
