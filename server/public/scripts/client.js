469615eb8a44cfcacb724f73e5e9971ebb860600
James Nguyen <33408161+Zygy93@users.noreply.github.com>
2018-02-08 14:53:20 -0600

    Update client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,19 +1,10 @@
 $(document).ready(onReady);
 
 function onReady(){
-  // indexAppend();
+  indexAppend();
     $('#submitGuessBtn').on('click', guessCounter);
 }
-
 var guessesMade = 0;
-function indexAppend(){
-    console.log('Appender Function');
-    var $gameUI = $('#gameUI');
-    var stringToAppend = '';
-    $gameUI.empty();
-    stringToAppend += `<p>I can do what I want</p>` // Add to DOM, copy this, paste below
-    $gameUI.append(stringToAppend);
-}
 function guessCounter() {
     guessesMade++;
     var createCountDiv = '<div id=guessCountDiv>';
@@ -27,8 +18,6 @@ function guessCounter() {
     $('#guessCountDiv').append(totalGuesses);
 } // end guess counter
 
-   indexAppend();
-}
 function indexAppend(){
    console.log(‘Appender Function’);
    var $gameUI = $(‘#gameUI’);
