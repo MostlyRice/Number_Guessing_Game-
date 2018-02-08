aa5eb10b26a9d5dfd2e412ae8dafb4a6d85f1708
Daniel Shugarts <daniel.shugarts@gmail.com>
2018-02-08 14:45:09 -0600

    guess counter complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,31 @@
 $(document).ready(onReady);
 
 function onReady(){
-    indexAppend();
+  // indexAppend();
+    $('#submitGuessBtn').on('click', guessCounter);
 }
 
-function indexAppend(){
+var guessesMade = 0;
+
+ /*function indexAppend(){
     console.log('Appender Function');
     var $gameUI = $('#gameUI');
     var stringToAppend = '';
     $gameUI.empty();
     stringToAppend += `<p>I can do what I want</p>` // Add to DOM, copy this, paste below
     $gameUI.append(stringToAppend);
-}
+}*/
+
+function guessCounter() {
+    guessesMade++;
+    var createCountDiv = '<div id=guessCountDiv>';
+    createCountDiv += '</div>';
+    $('#gameUI').append(createCountDiv);
+    $('#guessCountDiv').empty();
+    var totalGuesses = '<h4>';
+    totalGuesses += 'Number of Guesses: ';
+    totalGuesses += guessesMade;
+    totalGuesses += '</h4>';
+    $('#guessCountDiv').append(totalGuesses);
+} // end guess counter
 
