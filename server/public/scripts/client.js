c1d87e94a64033f98edf677720e9ef192681d0d7
Zygy93 <amazinzygy@gmail.com>
2018-02-08 14:30:03 -0600

    added generator client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,32 @@
 $(document).ready(onReady);
 
 function onReady(){
-    indexAppend();
+   indexAppend();
 }
-
 function indexAppend(){
-    console.log('Appender Function');
-    var $gameUI = $('#gameUI');
-    var stringToAppend = '';
-    $gameUI.empty();
-    stringToAppend += `<p>I can do what I want</p>` // Add to DOM, copy this, paste below
-    $gameUI.append(stringToAppend);
-}
-
+   console.log(‘Appender Function’);
+   var $gameUI = $(‘#gameUI’);
+   var stringToAppend = ‘’;
+   $gameUI.empty();
+   stringToAppend += `<h1>Number Guessing Game</h1>`
+   stringToAppend += `<div class="playerOne">`
+   stringToAppend += `<p>Player 1<p>`
+   stringToAppend += `<input type="text" class='guessInput' placeholder="Player 1 Guess">`
+   stringToAppend += `</div>`
+   stringToAppend += `<div class="playerTwo">`
+   stringToAppend += `<p>Player 2<p>`
+   stringToAppend += `<input type="text" class='guessInput' placeholder="Player 2 Guess">`
+   stringToAppend += `</div>`
+   stringToAppend += `<div class="playerThree">`
+   stringToAppend +=  `<p>Player 3<p>`
+   stringToAppend += `<input type="text" class='guessInput' placeholder="Player 3 Guess">`
+   stringToAppend += `</div>`
+   stringToAppend += `<div class="playerFour">`
+   stringToAppend += `<p>Player 4<p>`
+   stringToAppend += `<input type="text" class='guessInput' placeholder="Player 4 Guess">`
+   stringToAppend += `</div>`
+   stringToAppend += `</br>`
+   stringToAppend += `<button id="submitGuessBtn">Submit Guesses</button>`
+   stringToAppend += `<button id="cancelBtn">Cancel Guesses</button>`
+   $gameUI.append(stringToAppend);
+} // end indexAppend
